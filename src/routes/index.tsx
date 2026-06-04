dbf35d938abb21e9bdcf4cbebae4044e3abdb107
Vincenthao84 <vincenthao84@gmail.com>
2026-06-05 03:02:10 +0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,21 +7,7 @@ import { SiteHeader, SiteFooter } from "@/components/SiteHeader";
 import { requestTypes } from "@/lib/request-types";
 import { useAuth } from "@/hooks/use-auth";
 
-// 1. Import the image at the top of the component file
-import plantLogo from './assets/plant-logo.png'; // Adjust the '../' path depending on where your file is located
-
-export default function Navbar() {
-  return (
-    <div className="flex items-center">
-      {/* 2. Use the imported variable directly in the src attribute */}
-      <img 
-        src={plantLogo} 
-        alt="PlanT Logo" 
-        className="h-8 w-auto" 
-      />
-    </div>
-  );
-}
+<img src="/plant-logo.png" alt="PlanT Logo" />
 
 
 
